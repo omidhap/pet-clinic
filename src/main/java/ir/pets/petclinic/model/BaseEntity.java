c9f97bbf27f1c82df16d0bc75cffd52fce28c53e
omid_hap <ohajabdolpoor@gmail.com>
2019-02-10 00:43:58 +0330

    getter and setters deleted

--- src/main/java/ir/pets/petclinic/model/BaseEntity.java
+++ src/main/java/ir/pets/petclinic/model/BaseEntity.java
@@ -15,12 +15,4 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- src/main/java/ir/pets/petclinic/model/Pet.java
+++ src/main/java/ir/pets/petclinic/model/Pet.java
@@ -13,27 +13,4 @@ public class Pet extends BaseEntity{
     private Owner owner;
     private LocalDate birthDate;
 
-    public String getPetType() {
-        return petType;
-    }
-
-    public void setPetType(String petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 }
