2f572093fe0ee14c5f7bcbb1eb2230eb750f969c
omid_hap <ohajabdolpoor@gmail.com>
2019-02-10 00:42:10 +0330

    base entity id definition

--- src/main/java/ir/pets/petclinic/model/BaseEntity.java
+++ src/main/java/ir/pets/petclinic/model/BaseEntity.java
@@ -3,11 +3,17 @@ package ir.pets.petclinic.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @Entity
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
